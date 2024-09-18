70c259a04d7945196c2147080915ca334ae58f16
Cong Dinh <congdinh2012@hotmail.com>
2024-09-18 18:20:53 +0700

    Create base project

---
+++ src/main/java/com/congdinh/vivustore/VivustoreApplication.java
@@ -0,0 +1,13 @@
+package com.congdinh.vivustore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivustoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivustoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/congdinh/vivustore/VivustoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congdinh.vivustore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivustoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
